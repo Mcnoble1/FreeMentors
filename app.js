4df84c4b354a8529e4bcaa88bb65342961c4e4f2
mcnoble1 <idowufestustemiloluwa@gmail.com>
2025-04-14 15:05:51 +0100

    chore: setup database connection :octocat:
    [fixes #1]

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import pool from './config/db.js';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
@@ -18,5 +22,19 @@ app.use(express.static(path.join(dirname(fileURLToPath(import.meta.url)), 'publi
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+pool.connect()
+  .then(client => {
+    console.log('✅ PostgreSQL connected successfully');
+    client.release();
+  })
+  .catch(err => {
+    console.error('❌ PostgreSQL connection error:', err.stack);
+  });
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`🚀 Server running on port ${process.env.SERVER_PORT}`);
+});
+
 
 export default app;
