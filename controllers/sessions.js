ee77083961f1248a492b298cd8003558f8bc8432
Olalekan <lekanogundele@gmail.com>
2025-04-17 16:53:54 +0100

    Sessions Completed

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -39,3 +39,81 @@ export const createSession = async (req, res) => {
     }
 }
 
+export const acceptSession = async (req, res) => {
+    try {
+        if (req.user.role != 'mentor'){
+            return res.status(403).json({
+                status: 403,
+                error: 'You are not allowed to update a session',
+            });
+        }
+        
+        const sessionId = req.params.sessionId
+        const currentSession = Session.findSessionById(sessionId)
+        const questions = currentSession.questions
+        const session = await Session.updateSession(sessionId, {
+            questions,
+            status: 'accepted'
+        })
+
+        res.status(200).json({
+            status: 200,
+            message: 'Session accepted successfully',
+            data: session,
+            });
+    } catch (error) {
+        res.status(500).json({ status: 500, error: error.message });
+    }
+}
+
+export const declineSession = async (req, res) => {
+    try {
+        if (req.user.role != 'mentor'){
+            return res.status(403).json({
+                status: 403,
+                error: 'You are not allowed to update a session',
+            });
+        }
+        
+        const sessionId = req.params.sessionId
+        const currentSession = Session.findSessionById(sessionId)
+        const questions = currentSession.questions
+        const session = await Session.updateSession(sessionId, {
+            questions,
+            status: 'declined'
+        })
+        res.status(200).json({
+            status: 200,
+            message: 'Session declined successfully',
+            data: session,
+            });
+    } catch (error) {
+        res.status(500).json({ status: 500, error: error.message });
+    }
+}
+
+export const getSessions = async (req, res) => {
+    try {
+        if (req.user.role === 'user'){
+            const menteeId = req.user.id;
+            const session = Session.findAllMenteeSessions(menteeId)
+            res.status(200).json({
+                status: 200,
+                message: 'Sessions gotten successfully',
+                data: session,
+                });
+        }
+        else{
+            const mentorId = req.user.id;
+            const session = Session.findAllMentorSessions(mentorId)
+            res.status(200).json({
+                status: 200,
+                message: 'Sessions gotten successfully',
+                data: session,
+                });
+        }
+
+    } catch (error) {
+        res.status(500).json({ status: 500, error: error.message });
+    }
+}
--- routes/sessions.js
+++ routes/sessions.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { sessionMiddleware } from '../middlewares/user.js';
 import { auth, isAdmin } from '../middlewares/auth.js';
-import { createSession } from '../controllers/sessions.js';
+import { createSession, acceptSession, declineSession, getSessions } from '../controllers/sessions.js';
 
 const router = express.Router();
 
 router.post('/', auth, sessionMiddleware, createSession);
+router.post('/:sessionId/accept', auth, acceptSession);
+router.post('/:sessionId/decline', auth, declineSession);
+router.get('/', auth, sessionMiddleware, getSessions);
 
 export default router;
